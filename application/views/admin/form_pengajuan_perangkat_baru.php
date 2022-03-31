5a533b5451fbcf16e8f97172d7d37e9cf22dd229
deniekapratama <deniekapratama07@gmail.com>
2022-03-31 15:27:45 +0700

    updaye

--- application/views/admin/form_pengajuan_perangkat_baru.php
+++ application/views/admin/form_pengajuan_perangkat_baru.php
@@ -13,7 +13,7 @@
                       <div class="col-12">
                           <div class="card">
                               <div class="card-header">
-                                  <h4>Simple Summernote</h4>
+                                  <h4><?php echo $title ?></h4>
                               </div>
                               <div class="card-body">
                                   <div class="form-group row mb-4">
@@ -61,16 +61,6 @@
                                       </div>
                                   </div>
                                   <div class="form-group row mb-4">
-                                      <label class="col-form-label text-md-right col-12 col-md-3 col-lg-3">Jenis Permintaan</label>
-                                      <div class="col-sm-12 col-md-7">
-                                          <select class="form-control selectric">
-                                              <option>Tech</option>
-                                              <option>News</option>
-                                              <option>Political</option>
-                                          </select>
-                                      </div>
-                                  </div>
-                                  <div class="form-group row mb-4">
                                       <label class="col-form-label text-md-right col-12 col-md-3 col-lg-3">No Surat NDE</label>
                                       <div class="col-sm-12 col-md-7">
                                           <input type="text" class="form-control">
@@ -82,7 +72,17 @@
                                           <input type="text" class="form-control">
                                       </div>
                                   </div>
-                                  
+
+                                  <div class="form-group row mb-4">
+                                      <label class="exampleFormControlFile1 text-md-right col-12 col-md-3 col-lg-3">Lampiran File PDF Surat (NDE)</label>
+                                      <div class="col-sm-12 col-md-7">
+                                          <input type="file" class="form-control-file" id="exampleFormControlFile1">
+                                      </div>
+                                  </div>
+
+                                  <div class="form-group row m-right">
+                                      <button type="button" class="btn btn-primary">Primary</button>
+                                  </div>
                               </div>
                           </div>
                       </div>
