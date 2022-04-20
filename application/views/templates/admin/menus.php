6ffe6df9e613fbcc385215e43e612b4eaf1df285
deniekapratama <deniekapratama07@gmail.com>
2022-04-20 21:59:02 +0700

    menus

--- application/views/templates/admin/menus.php
+++ application/views/templates/admin/menus.php
@@ -34,6 +34,7 @@
                     </ul>
                 </ul>
             </li>
+            <li class="menu-header">Data</li>
             <li class="dropdown">
                 <a href="#" class="nav-link has-dropdown"><i class="fas fa-th-large"></i> <span>Pengajuan</span></a>
                 <ul class="dropdown-menu">
@@ -175,17 +176,25 @@
 
             <li class="menu-header">Personal</li>
             <li class="dropdown">
-                <a href="#" class="nav-link has-dropdown"><i class="fas fa-desktop"></i> <span>Perangkat Saya</span></a>
+                <a href="#" class="nav-link has-dropdown"><i class="fas fa-laptop"></i> <span>Perangkat Saya</span></a>
                 <ul class="dropdown-menu">
                     <li><a href="auth-forgot-password.html">Pengajuan Perangkat</a></li>
                     <li><a href="auth-forgot-password.html">Daftar Perangkat Saya</a></li>
+                </ul>
+            </li>
+            <li class="menu-header">Report</li>
+            <li class="dropdown">
+                <a href="#" class="nav-link has-dropdown"><i class="fas fa-chart-bar"></i> <span>Laporan</span></a>
+                <ul class="dropdown-menu">
+                    <li><a href="auth-forgot-password.html">Pengajuan</a></li>
+                    <li><a href="auth-forgot-password.html">Pengembalian </a></li>
                 </ul>
             </li>
             <li><a class="nav-link" href="credits.html"><i class="fas fa-pencil-ruler"></i> <span>Credits</span></a></li>
         </ul>
 
         <div class="mt-4 mb-4 p-3 hide-sidebar-mini">
-            <a href=" https://getstisla.com/docs" class="btn btn-primary btn-lg btn-block btn-icon-split">
+            <a href=" https://getstisla.com/docs" target="_blank" class="btn btn-primary btn-lg btn-block btn-icon-split">
                 <i class="fas fa-rocket"></i> Documentation
             </a>
         </div>
