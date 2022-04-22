64df354693ccf491231779c0227c91661444e2af
deniekapratama <deniekapratama07@gmail.com>
2022-04-23 01:13:26 +0700

    update config

--- application/config/config.php
+++ application/config/config.php
@@ -26,8 +26,8 @@ defined('BASEPATH') or exit('No direct script access allowed');
 $root = "http://" . $_SERVER['HTTP_HOST'];
 $root .= str_replace(basename($_SERVER['SCRIPT_NAME']), "", $_SERVER['SCRIPT_NAME']);
 
-$config['base_url'] = 'http://ec2-54-255-243-105.ap-southeast-1.compute.amazonaws.com/';
-
+// $config['base_url'] = 'http://ec2-54-255-243-105.ap-southeast-1.compute.amazonaws.com/';
+$config['base_url'] = 'http://' . $_SERVER['HTTP_HOST'] . '';
 /*
 |--------------------------------------------------------------------------
 | Index File
