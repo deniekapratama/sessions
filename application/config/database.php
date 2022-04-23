ea0632c0e2286ee36fe95bc14d6cdb114d6862a7
deniekapratama <deniekapratama07@gmail.com>
2022-04-23 13:52:36 +0700

    updae

--- application/config/database.php
+++ application/config/database.php
@@ -72,6 +72,7 @@ defined('BASEPATH') or exit('No direct script access allowed');
 */
 $active_group = 'default';
 $query_builder = TRUE;
+$active_record = TRUE;
 
 $db['default'] = array(
 	'dsn'	=> '',
