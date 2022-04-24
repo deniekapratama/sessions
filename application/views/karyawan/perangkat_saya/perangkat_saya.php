4af472b1d9f977c09697e47911aaa0fd330325e6
deniekapratama <deniekapratama07@gmail.com>
2022-04-24 15:46:45 +0700

    update

--- application/views/karyawan/perangkat_saya/perangkat_saya.php
+++ application/views/karyawan/perangkat_saya/perangkat_saya.php
@@ -67,8 +67,8 @@
                                                             Pilih
                                                         </button>
                                                         <div class="dropdown-menu">
-                                                            <a class="dropdown-item" data-toggle="modal" id="detail">Detail</a>
-                                                            <a class="dropdown-item" href="#">Kembalikan</a>
+                                                            <a class="dropdown-item" data-toggle="modal" id="detail" href="#">Detail</a>
+                                                            <a class="dropdown-item" data-toggle="modal" data-target="#exampleModal">Kembalikan</a>
                                                         </div>
                                                     </div>
                                                 </td>
@@ -129,52 +129,52 @@
                     <div class="form-group row">
                         <label for="staticEmail" class="col-sm-2 col-form-label">Nama Kantor</label>
                         <div class="col-sm-6">
-                            <input type="text" readonly class="form-control-plaintext" id="staticEmail" value=" <?php echo $namaktr; ?>">
+                            <input type="text" readonly class="form-control-plaintext" id="staticEmail" value=": <?php echo $namaktr; ?>">
                         </div>
                     </div>
                     <div class="form-group row">
                         <label for="staticEmail" class="col-sm-2 col-form-label">Bagian</label>
                         <div class="col-sm-6">
-                            <input type="text" readonly class="form-control-plaintext" id="staticEmail" value="<?php echo $namabagian; ?>">
+                            <input type="text" readonly class="form-control-plaintext" id="staticEmail" value=": <?php echo $namabagian; ?>">
                         </div>
                     </div>
 
                     <div class="form-group row">
                         <label for="staticEmail" class="col-sm-2 col-form-label">Jabatan</label>
                         <div class="col-sm-6">
-                            <input type="text" readonly class="form-control-plaintext" id="staticEmail" value="<?php echo $jabatan; ?>">
+                            <input type="text" readonly class="form-control-plaintext" id="staticEmail" value=": <?php echo $jabatan; ?>">
                         </div>
                     </div>
 
                     <div class="form-group row">
                         <label for="staticEmail" class="col-sm-2 col-form-label">Merk</label>
                         <div class="col-sm-6">
-                            <input type="text" readonly class="form-control-plaintext" id="staticEmail" value=" <?php echo $merk; ?>">
+                            <input type="text" readonly class="form-control-plaintext" id="staticEmail" value=": <?php echo $merk; ?>">
                         </div>
                     </div>
 
                     <div class="form-group row">
                         <label for="staticEmail" class="col-sm-2 col-form-label">Spefisikasi</label>
                         <div class="col-sm-6">
-                            <input type="text" readonly class="form-control-plaintext" id="staticEmail" value=" <?php echo $spesifikasi; ?>">
+                            <input type="text" readonly class="form-control-plaintext" id="staticEmail" value=": <?php echo $spesifikasi; ?>">
                         </div>
                     </div>
                     <div class="form-group row">
                         <label for="staticEmail" class="col-sm-2 col-form-label">Serial Number</label>
                         <div class="col-sm-6">
-                            <input type="text" readonly class="form-control-plaintext" id="staticEmail" value=" <?php echo $serial_number; ?>">
+                            <input type="text" readonly class="form-control-plaintext" id="staticEmail" value=": <?php echo $serial_number; ?>">
                         </div>
                     </div>
                     <div class="form-group row">
                         <label for="staticEmail" class="col-sm-2 col-form-label">Jenis Perangkat</label>
                         <div class="col-sm-6">
-                            <input type="text" readonly class="form-control-plaintext" id="staticEmail" value=" <?php echo $jenis_perangkat; ?>">
+                            <input type="text" readonly class="form-control-plaintext" id="staticEmail" value=": <?php echo $jenis_perangkat; ?>">
                         </div>
                     </div>
                     <div class="form-group row">
                         <label for="staticEmail" class="col-sm-2 col-form-label">Lampiran</label>
                         <div class="col-sm-6">
-                            <input type="text" readonly class="form-control-plaintext" id="staticEmail" value=" : Lampiran Serah Terima">
+                            <input type="text" readonly class="form-control-plaintext" id="staticEmail" value=": Lampiran Serah Terima">
                         </div>
                     </div>
 
@@ -189,7 +189,6 @@
         </div>
     </div>
 </div>
-
 
 
 <!-- Modal -->
@@ -211,12 +210,13 @@
                 </div>
             </div>
             <div class="modal-footer">
-                <button type="button" class="btn btn-secondary" data-dismiss="modal">Batal</button>
-                <button type="button" class="btn btn-primary">Cetak Pengajuan</button>
+                <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+                <button type="button" class="btn btn-primary">Save changes</button>
             </div>
         </div>
     </div>
 </div>
+
 
 
 
