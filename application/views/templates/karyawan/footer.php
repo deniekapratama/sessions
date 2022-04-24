7719605f567e6c0a4cf8773a9ce0103d2c544d33
deniekapratama <deniekapratama07@gmail.com>
2022-04-24 23:38:28 +0700

    footter update

--- application/views/templates/karyawan/footer.php
+++ application/views/templates/karyawan/footer.php
@@ -1,6 +1,6 @@
 <footer class="main-footer">
     <div class="footer-left">
-        Copyright &copy; 2022 <div class="bullet"></div> Design By <a href="https://nauval.in/">Ganesha Corp.</a>
+    Copyright &copy; 2022 <div class="bullet"></div> Design & Development By <a href="https://posindonesia.co.id/">Ganesha Corp.</a>
     </div>
     <div class="footer-right">
 
