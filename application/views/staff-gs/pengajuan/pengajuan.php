354566f6f74daded3a2a3359f63b4dd4283fb41a
athian fardan <athianfardan@gmail.com>
2022-04-26 11:37:23 +0700

    ts

--- application/views/staff-gs/pengajuan/pengajuan.php
+++ application/views/staff-gs/pengajuan/pengajuan.php
@@ -5,7 +5,7 @@
             <h1><?= $title ?></h1>
             <div class="section-header-breadcrumb">
                 <div class="breadcrumb-item active"><a href="#">Dashboard</a></div>
-                <div class="breadcrumb-item">Pengajuan Perangkat Baru</a></div>
+                <div class="breadcrumb-item">Pengajuan Perangkat Baru2</a></div>
             </div>
         </div>
 
