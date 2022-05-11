f62f26dd01172f9f6c6d6b7788ed9c1557d4c3ba
athian fardan <athianfardan@gmail.com>
2022-05-11 15:11:54 +0700

    api login

--- application/models/Model_login.php
+++ application/models/Model_login.php
@@ -28,42 +28,24 @@ class Model_login extends CI_Model
             $curl = curl_init();
 
             curl_setopt_array($curl, array(
-                CURLOPT_URL => 'https://psis.posindonesia.co.id/simsdm/public/1.0.0/aset/login',
-                CURLOPT_RETURNTRANSFER => true,
-                CURLOPT_ENCODING => '',
-                CURLOPT_MAXREDIRS => 10,
-                CURLOPT_TIMEOUT => 0,
-                CURLOPT_FOLLOWLOCATION => true,
-                CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
-                CURLOPT_CUSTOMREQUEST => 'POST',
-                CURLOPT_HTTPHEADER => array(
-                'nippos: '.$username,
-                'password: '.$password
-            ),
+              CURLOPT_URL => 'http://10.60.64.55/simsdm/public/1.0.0/aset/login',
+              CURLOPT_RETURNTRANSFER => true,
+              CURLOPT_ENCODING => '',
+              CURLOPT_MAXREDIRS => 10,
+              CURLOPT_TIMEOUT => 0,
+              CURLOPT_FOLLOWLOCATION => true,
+              CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+              CURLOPT_CUSTOMREQUEST => 'POST',
+              CURLOPT_HTTPHEADER => array(
+                'nippos: 972336000',
+                'password: veni999'
+              ),
             ));
             
             $response = curl_exec($curl);
-            
-            curl_close($curl);
-            $data1 = json_decode($response,TRUE);
-            $nippospeg = $data1['data'][0]['nippos'];
-            $namapeg = $data1['data'][0]['nama'];
-            $nopend = $data1['data'][0]['nopend'];
-            $namaktr = $data1['data'][0]['namaktr'];
-            $kodebagian = $data1['data'][0]['kodebagian'];
-            $namabagian = $data1['data'][0]['namabagian'];
-            $kodejabatan = $data1['data'][0]['jabatan'];
-            $namajabatan = $data1['data'][0]['descjabatan'];
-            $this->session->set_userdata('nippos', $nippospeg);
-            $this->session->set_userdata('nama', $namapeg);
-            $this->session->set_userdata('nopend', $nopend);
-            $this->session->set_userdata('namaktr', $namaktr);
-            $this->session->set_userdata('kodebagian', $kodebagian);
-            $this->session->set_userdata('namabagian', $namabagian);
-            $this->session->set_userdata('kodejabatan', $kodejabatan);
-            $this->session->set_userdata('namajabatan', $namajabatan);
 
-            return $response;
+curl_close($curl);
+return $response;
         }
             // return "admin";
     }
